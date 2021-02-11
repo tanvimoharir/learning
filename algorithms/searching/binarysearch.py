1d6a602d805a8f15c55cb9d4558a4c0e84179183
tanvimoharir <tanvimoharir@gmail.com>
2021-02-11 10:46:54 +0530

    Format

--- algorithms/searching/binarysearch.py
+++ algorithms/searching/binarysearch.py
@@ -15,14 +15,13 @@ def bs(array, target):
     end = len(array) - 1
     while start <= end:
         mid = (start + end) // 2
-        if array[mid]==target:
+        if array[mid] == target:
             return mid
-        elif array[mid]<target:
-            start=mid+1
-        elif array[mid]>target:
-            end=mid-1
+        elif array[mid] < target:
+            start = mid + 1
+        elif array[mid] > target:
+            end = mid - 1
     return -1
-        
 
 
 print(bs(array=[1, 2, 3, 4, 6, 7, 9], target=5))
