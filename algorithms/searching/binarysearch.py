a24f1c86976ba8608fbc564dd448695744223bed
tanvimoharir <tanvimoharir@gmail.com>
2021-02-11 09:51:16 +0530

    Binary iterative

--- algorithms/searching/binarysearch.py
+++ algorithms/searching/binarysearch.py
@@ -1,12 +1,28 @@
-
-def binarysearch(array,target,start,end):
-    mid=(start+end)//2
-    if array[mid]==target:
+def binarysearch(array, target, start, end):
+    # recursive
+    mid = (start + end) // 2
+    if array[mid] == target:
         return mid
-    elif array[mid]>target:
-        return binarysearch(array,target,start,mid-1)
-    elif array[mid]<target:
-        return binarysearch(array,target,mid+1,end)
+    elif array[mid] > target:
+        return binarysearch(array, target, start, mid - 1)
+    elif array[mid] < target:
+        return binarysearch(array, target, mid + 1, end)
 
 
-print(binarysearch(array=[1,2,3,4,6,7,9],target=2,start=0,end=6))
+def bs(array, target):
+    # iterative
+    start = 0
+    end = len(array) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid]==target:
+            return mid
+        elif array[mid]<target:
+            start=mid+1
+        elif array[mid]>target:
+            end=mid-1
+    return -1
+        
+
+
+print(bs(array=[1, 2, 3, 4, 6, 7, 9], target=5))
